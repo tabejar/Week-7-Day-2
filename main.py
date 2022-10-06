bf850ba88bf0ba7c8d721c5a05fda7174f87461a
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-10-06 16:29:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# fork the repl.it and create a new git hub repo entitled
+#Comparison operators
+# Logical operators
+# Decision making
+# loops(for loops, while loops, range, enumerator)
+# min/max practice
+# Random in python
+# List comprehension
+#importing modules
+#modularized your code 
+#work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# from dictionaries_loops import dictionaries_loops
+from random import random
+from loops import loop
+from ranges import range
+from enumerators import enum
+from zip import zip
+from min_max import min_max
+# decisions()
+# comparison()
+loop()
+# random()
+# range()
+# enum()
+# zip()
+# min_max()
